2f1401f3ced20a63d1c779442c0ca6c6b0fb4771
AliAhmadiNasab <aliahmadinasab81@gmail.com>
2025-03-02 23:24:40 +0330

    close the game with ESC key

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from pygame.locals import *
 if __name__ == "__main__":
     pygame.init()
     
-surface = pygame.display.set_mode((400, 400))
-surfac.fill((255,255,255))
+surface = pygame.display.set_mode((1000, 500))
+surface.fill((110,110,5))
 pygame.display.flip()
 
 running = True
+
 while running:
     for event in pygame.event.get():
         if event.type == KEYDOWN:
-            pass
+            if event.key == K_ESCAPE:
+                running = False
         elif event.type == QUIT:
             running = False
